2457c53fc59b21f4f3a5c195ef1f1db92477d5ae
squyz <matveevevgenij77@gmail.com>
2023-09-23 11:34:10 +0300

    day1.1

---
+++ src/components/PostForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <form @submit.prevent>
+        <h4>Создание поста</h4>
+        <input
+            v-bind:value="post.title"
+            @input="post.title = $event.target.value"
+            class="input"
+            type="text"
+            placeholder="Название"
+        >
+        <input
+            v-bind:value="post.body"
+            @input="post.body = $event.target.value"
+            class="input" 
+            type="text" 
+            placeholder="Описание"
+        >
+        <button class="btn">Создать</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            post: {
+                title: '', 
+                body: ''
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+form{
+    display: flex;
+    flex-direction: column;
+}
+.input{
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+    background-color: white;
+}
+.btn{
+    align-self: flex-end;
+    margin-top: 15px;
+    padding: 10px 15px;
+    color: teal;
+    border: 1px solid teal;
+    background-color: white;
+}
+</style>
